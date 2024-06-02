935d61baef10fb4470c480db78ff342e5a148835
dropshotcaraz <ygscavanger@gmail.com>
2024-06-02 18:03:38 +0700

    addlainlain

---
+++ detailproduk.go
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rahayu Production</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&" type="image/x-icon" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="script.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {
+            fontFamily: {
+              'poppins': ['Poppins', 'sans-serif'],
+            },
+            colors: {
+              clifford: "#da373d",
+              saudagar: "#0088da",
+            },
+            fontSize: {
+              '16': '16px',
+              '20': '20px',
+              '24': '24px',
+              '28': '28px',
+              '32': '32px',
+            },
+          },
+        },
+      }
+    </script>
+    <style>
+      body {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+      }
+
+      main {
+        flex: 1;
+      }
+    </style>
+  </head>
+
+  <body class="bg-gray-50 font-poppins">
+    <header class="bg-slate-900 sticky top-0 z-50">
+      <div class="container mx-auto flex justify-between items-center p-5"> <a href="index.html">
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/63fbd06a3df914182d7460b662cbf599153f13a0878d942096f2ffd365abe5fc?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&" alt="Icon 1">
+      </a><div class="flex gap-5"> <a href="alurpesan.html">
+          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/3cc4aa42302637e6a3bb097eadab7854b3163efca371bc9b60b3232fb1990b44?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&" alt="Icon 1" class="w-6 h-6">
+        </a><a href="cekorder.html"><img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/78a27e6dfb55f7636f9e721bafe5998c83d471d5f36470deda76a806717006c1?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&" alt="Icon 2" class="w-6 h-6">
+        </a></div>
+      </div>
+    </header>
+    <main class="container mx-auto mt-8 max-sm:mb-8 text-center">
+      <div class="flex flex-col pb-16 bg-stone-300">
+        <div
+          class="flex gap-5 px-20 py-4 w-full bg-teal-950 max-md:flex-wrap max-md:px-5 max-md:max-w-full"
+        >
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/d6c412e527072d3eb8bf2ed2edc22eb855e6b571df4f0c796d4e208f3fb0e762?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&"
+            class="shrink-0 my-auto aspect-square w-[52px]"
+          />
+          <img
+            loading="lazy"
+            srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/36cf3edf39376f86c04999cb0b171393b2c1025d9965f5bdec82a5ad53dc8d0c?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/36cf3edf39376f86c04999cb0b171393b2c1025d9965f5bdec82a5ad53dc8d0c?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/36cf3edf39376f86c04999cb0b171393b2c1025d9965f5bdec82a5ad53dc8d0c?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/36cf3edf39376f86c04999cb0b171393b2c1025d9965f5bdec82a5ad53dc8d0c?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/36cf3edf39376f86c04999cb0b171393b2c1025d9965f5bdec82a5ad53dc8d0c?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/36cf3edf39376f86c04999cb0b171393b2c1025d9965f5bdec82a5ad53dc8d0c?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/36cf3edf39376f86c04999cb0b171393b2c1025d9965f5bdec82a5ad53dc8d0c?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/36cf3edf39376f86c04999cb0b171393b2c1025d9965f5bdec82a5ad53dc8d0c?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&"
+            class="shrink-0 max-w-full aspect-[2.04] w-[181px]"
+          />
+        </div>
+        <div
+          class="flex flex-col items-end self-center mt-20 -ml-1 w-full font-bold max-w-[1100px] max-md:mt-10 max-md:max-w-full"
+        >
+          <div
+            class="flex gap-5 self-start max-w-full text-3xl tracking-wide text-black w-[747px] max-md:flex-wrap"
+          >
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/cdf11d8ff53db961fd08fd0bb4b3a950ae3a43a8e6fca5bba1f34820844ea8a1?apiKey=f696dd3e51664ad181dc4a1c50d0b69b&"
+              class="shrink-0 w-8 border-black border-solid aspect-[1.14] stroke-[2px] stroke-black max-sm:ml-5"
+            />
+            <div class="flex-auto">Detail Produk</div>
+          </div>
+          <div
+            class="flex flex-col items-center pb-16 mt-12 -ml-px max-w-full bg-white rounded-xl w-[1138px]"
+          >
+            <div data-missing-component="BuilderCarousel"></div>
+            <div
+              class="justify-center items-center px-16 py-8 mt-24 max-w-full text-xl font-semibold tracking-wide text-center rounded-none bg-stone-300 text-teal-950 w-[650px] max-md:px-5 max-md:mt-10 max-sm:p-5 max-sm:m-2.5 max-sm:w-[461px]"
+            >
+              Geser untuk melihat koleksi lainnya
+            </div>
+            <div class="mt-16 text-4xl tracking-wide text-black max-md:mt-10">
+              Kemeja
+            </div>
+            <div class="mt-4 text-xl tracking-wide text-black">Mulai dari</div>
+            <div
+              class="flex flex-col self-stretch px-12 mt-9 max-md:px-5 max-md:max-w-full max-sm:text-center"
+            >
+              <div
+                class="flex gap-5 w-full text-lg tracking-wide text-center text-white whitespace-nowrap max-md:flex-wrap max-md:max-w-full"
+              >
+                <div class="flex flex-1 flex-auto gap-0 self-start">
+                  <div
+                    class="justify-center items-center px-16 py-7 rounded-md bg-teal-950 max-md:px-5 max-sm:ml-auto"
+                  >
+                    Reguler
+                  </div>
+                  <div
+                    class="justify-center items-start px-16 py-7 rounded-none bg-neutral-400 max-md:pr-5 max-md:pl-7 max-sm:pl-6 max-sm:mr-auto"
+                  >
+                    120k
+                  </div>
+                </div>
+                <div class="flex flex-1 flex-auto gap-0">
+                  <div
+                    class="justify-center items-center px-16 py-7 ml-auto rounded-md bg-teal-950 max-md:px-5 max-sm:ml-auto"
+                  >
+                    Premium
+                  </div>
+                  <div
+                    class="justify-center items-start px-16 py-8 bg-amber-200 rounded-none max-md:pr-5 max-md:pl-6 max-sm:mr-auto max-sm:-ml-px"
+                  >
+                    140k
+                  </div>
+                </div>
+              </div>
+              <div
+                class="mt-16 text-3xl tracking-wide text-black max-md:mt-10 max-md:max-w-full max-sm:text-2xl"
+              >
+                Deskripsi
+              </div>
+              <div
+                class="mt-9 text-2xl text-slate-700 max-md:mr-2.5 max-md:max-w-full max-sm:text-base"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipisc elit. Fusce at nibh
+                non ipsum tincidunt vulputate sit amet non dui. non ipsum tincidunt
+                vulputate sit amet non dui.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </main>
+    <footer class="bg-slate-900 py-6 text-center text-white">
+      <p class="text-base">Created by Rahayu Production</p>
+    </footer>
+  </body>
+  <script async src='https://d2mpatx37cqexb.cloudfront.net/delightchat-whatsapp-widget/embeds/embed.min.js'>
+  </script>
+  <script>
+  var wa_btnSetting = {"btnColor":"#16BE45"
+  ,"ctaText":"Pesan Sekarang!",
+  "cornerRadius":40,
+  "marginBottom":20,
+  "marginLeft":20,
+  "marginRight":20,
+  "btnPosition":"left",
+  "whatsAppNumber":"6281295036566",
+  "welcomeMessage":"Saya Mau Memesan Produk Anda!"
+  ,"zIndex":999999,"btnColorScheme":"light"};
+  window.onload = () => {
+  _waEmbed(wa_btnSetting);
+  };
+  </script>
+</html>
